be9bf3b955bef81b606f842705578fdfbb036e6f
Nikait <50284221+Nikait@users.noreply.github.com>
2023-12-18 18:22:46 +0300

    Update 3-init.sql

--- scripts/3-init.sql
+++ scripts/3-init.sql
@@ -5,15 +5,14 @@ set search_path = demo_db;
 
 drop table if exists categories cascade;
 create table categories(
-    category_id           int not null primary key,
+	category_id           int not null primary key,
 	naming                text not null
 );
 
 
-
 drop table if exists supppliers cascade;
 create table supppliers(
-    supplier_id          int not null primary key,
+	supplier_id          int not null primary key,
 	naming                text not null,
 	rating                int check (rating between 0 and 10),
 	registration_addres   text not null,
@@ -21,12 +20,11 @@ create table supppliers(
 );
 
 
-
 drop table if exists goods cascade;
 create table goods(
-    good_id               int not null primary key,
-    category_id           int not null references categories(category_id),
-    supplier_id           int not null references supppliers(supplier_id),
+	good_id               int not null primary key,
+	category_id           int not null references categories(category_id),
+	supplier_id           int not null references supppliers(supplier_id),
 	naming                text not null,
 	rating                int not null check (rating between 1 and 10),
 	price                 int not null check (price >= 0),
@@ -34,20 +32,17 @@ create table goods(
 );
 
 
-
 drop table if exists suppplies_description cascade;
 create table suppplies_description(
-    suppply_id            int not null primary key,
+	suppply_id            int not null primary key,
 	supplier_id           int not null references supppliers(supplier_id),
 	date_of_supply        date
 );
 
 
-
-
 drop table if exists goods_in_supply cascade;
 create table goods_in_supply(
-    suppply_id            int not null,
+	suppply_id            int not null,
 	good_id               int not null,
 	quantity              int check (quantity >= 0),
 	primary key (suppply_id, good_id)
@@ -56,7 +51,7 @@ create table goods_in_supply(
 
 drop table if exists user_main cascade;
 create table user_main(
-    user_id               int not null primary key,
+	user_id               int not null primary key,
 	first_name            text not null,
 	surname               text not null,
 	address               text,
@@ -67,7 +62,7 @@ create table user_main(
 
 drop table if exists user_history cascade;
 create table user_history(
-    user_id               int not null primary key,
+	user_id               int not null primary key,
 	first_name            text not null,
 	surname               text not null,
 	address               text,
@@ -78,10 +73,9 @@ create table user_history(
 );
 
 
-
 drop table if exists purchases cascade;
 create table purchases(
-    purchase_id           int not null primary key,
+	purchase_id           int not null primary key,
 	user_id               int not null references user_main(user_id),
 	date_of_purchase      date
 );
@@ -90,7 +84,7 @@ create table purchases(
 
 drop table if exists item_in_purchases cascade;
 create table item_in_purchases(
-    purchase_id           int not null,
+	purchase_id           int not null,
 	product_id            int not null unique,
 	quantity              int check (quantity >= 0),
 	primary key (purchase_id, product_id)
@@ -99,13 +93,11 @@ create table item_in_purchases(
 
 drop table if exists reviews cascade;
 create table reviews(
-    review_id             int not null primary key,
-    user_id               int not null references user_main(user_id),
-    product_id            int not null references item_in_purchases(product_id),
+	review_id             int not null primary key,
+	user_id               int not null references user_main(user_id),
+	product_id            int not null references item_in_purchases(product_id),
 	rating                int not null check (rating between 0 and 10),
 	review                text not null,
 	date_of_review        date
 );
-
-
 
