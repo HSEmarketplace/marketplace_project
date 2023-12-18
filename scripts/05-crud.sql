3bb0deea96027cddaccea3394620faeac94b32a6
Nikait <50284221+Nikait@users.noreply.github.com>
2023-12-18 22:16:27 +0300

    Update 05-crud.sql

--- scripts/05-crud.sql
+++ scripts/05-crud.sql
@@ -1,12 +1,32 @@
 set search_path = demo_db, public;
 
+-- CRUD запросы
 
--- SELECT запрос для выбора отзывов и их товаров
-select r.review_id, r.review, r.rating, r.date_of_review, g.naming as product_name
-from reviews r
-inner join goods g on r.product_id = g.good_id;
+-- INSERT
+-- добавляем новые категорию и товар
+insert into categories (category_id, naming) 
+values (9, 'snack');
 
---- UPDATE запрос для изменения отзыва
-update reviews
-set rating = 9
-where review_id = 5;
+insert into goods (good_id, category_id, supplier_id, naming, rating, price, description)
+values (9, 5, 3, 'burn energy', 8, 1000, 'Burn is an energy drink owned and distributed by Monster Beverage Corporation, carrying the official tagline "Fuel your fire"');
+
+
+-- SELECT
+-- выбираем все категории и товары с рейтингом > 3
+select * from categories;
+
+select * from goods where rating > 3;
+
+
+-- UPDATE
+-- обновим название категории с id = 1 и устанавливаем максимальную ценуна товар - 100
+update categories set naming = 'game' where category_id = 1;
+
+update goods set price = 100 where price > 100;
+
+-- DELETE
+-- удаляем 9ую категорию и 9го пользователя
+delete from categories where category_id = 9;
+
+delete from goods where good_id = 9;
+
