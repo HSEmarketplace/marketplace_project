253c50b305bbea3434358ca184ac9e4b455e94ed
Nikait <50284221+Nikait@users.noreply.github.com>
2023-12-18 18:21:10 +0300

    Update 3-init.sql

--- scripts/3-init.sql
+++ scripts/3-init.sql
@@ -6,7 +6,7 @@ set search_path = demo_db;
 drop table if exists categories cascade;
 create table categories(
     category_id           int not null primary key,
-	  naming                text not null
+	naming                text not null
 );
 
 
@@ -14,10 +14,10 @@ create table categories(
 drop table if exists supppliers cascade;
 create table supppliers(
     supplier_id          int not null primary key,
-	  naming                text not null,
-	  rating                int check (rating between 0 and 10),
-	  registration_addres   text not null,
-	  contact_phone         text
+	naming                text not null,
+	rating                int check (rating between 0 and 10),
+	registration_addres   text not null,
+	contact_phone         text
 );
 
 
@@ -27,10 +27,10 @@ create table goods(
     good_id               int not null primary key,
     category_id           int not null references categories(category_id),
     supplier_id           int not null references supppliers(supplier_id),
-	  naming                text not null,
-	  rating                int not null check (rating between 1 and 10),
-	  price                 int not null check (price >= 0),
-	  description           text not null
+	naming                text not null,
+	rating                int not null check (rating between 1 and 10),
+	price                 int not null check (price >= 0),
+	description           text not null
 );
 
 
@@ -38,8 +38,8 @@ create table goods(
 drop table if exists suppplies_description cascade;
 create table suppplies_description(
     suppply_id            int not null primary key,
-	  supplier_id           int not null references supppliers(supplier_id),
-	  date_of_supply        date
+	supplier_id           int not null references supppliers(supplier_id),
+	date_of_supply        date
 );
 
 
@@ -48,33 +48,33 @@ create table suppplies_description(
 drop table if exists goods_in_supply cascade;
 create table goods_in_supply(
     suppply_id            int not null,
-  	good_id               int not null,
-  	quantity              int check (quantity >= 0),
-	  primary key (suppply_id, good_id)
+	good_id               int not null,
+	quantity              int check (quantity >= 0),
+	primary key (suppply_id, good_id)
 );
 
 
 drop table if exists user_main cascade;
 create table user_main(
     user_id               int not null primary key,
-	  first_name            text not null,
-	  surname               text not null,
-	  address               text,
-	  phone_number          text,
-	  email                 text
+	first_name            text not null,
+	surname               text not null,
+	address               text,
+	phone_number          text,
+	email                 text
 );
 
 
 drop table if exists user_history cascade;
 create table user_history(
     user_id               int not null primary key,
-  	first_name            text not null,
-	  surname               text not null,
-	  address               text,
-	  phone_number          text,
-	  email                 text,
-	  start_date            date,
-	  end_date              date
+	first_name            text not null,
+	surname               text not null,
+	address               text,
+	phone_number          text,
+	email                 text,
+	start_date            date,
+	end_date              date
 );
 
 
@@ -82,8 +82,8 @@ create table user_history(
 drop table if exists purchases cascade;
 create table purchases(
     purchase_id           int not null primary key,
-	  user_id               int not null references user_main(user_id),
-	  date_of_purchase      date
+	user_id               int not null references user_main(user_id),
+	date_of_purchase      date
 );
 
 
@@ -91,9 +91,9 @@ create table purchases(
 drop table if exists item_in_purchases cascade;
 create table item_in_purchases(
     purchase_id           int not null,
-	  product_id            int not null unique,
-	  quantity              int check (quantity >= 0),
-	  primary key (purchase_id, product_id)
+	product_id            int not null unique,
+	quantity              int check (quantity >= 0),
+	primary key (purchase_id, product_id)
 );
 
 
@@ -102,9 +102,9 @@ create table reviews(
     review_id             int not null primary key,
     user_id               int not null references user_main(user_id),
     product_id            int not null references item_in_purchases(product_id),
-	  rating                int not null check (rating between 0 and 10),
-	  review                text not null,
-	  date_of_review        date
+	rating                int not null check (rating between 0 and 10),
+	review                text not null,
+	date_of_review        date
 );
 
 
